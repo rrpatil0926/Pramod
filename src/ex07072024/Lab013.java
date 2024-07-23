be309901e6af586e9f1e1c3add7a82463e0d9322
Rashmi <rrpatil0926@gmail.com>
2024-07-23 23:55:29 +0530

    push to github

---
+++ src/ex07072024/Lab013.java
@@ -0,0 +1,14 @@
+package ex07072024;
+
+public class Lab013 {
+        // Can I run class without a main method?
+        // run option?
+        static public void  main(String[] args) {
+                System.out.println("with string");
+
+        }
+        public static void main() {
+                System.out.println("without string");
+
+        }
+}
